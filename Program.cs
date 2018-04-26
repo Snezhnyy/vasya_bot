daa96591d54a5b871832f54769b418f42d828374
Snezhnyy <anatoly.dobshikov@gmail.com>
2018-04-26 15:55:02 +0300

    Add bot logic

--- Program.cs
+++ Program.cs
@@ -1,15 +1,59 @@
 ﻿// Hello, Vasya
 
 using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
-namespace vasya_bot
+
+namespace Telegram.Bot.vasya_bot
 {
     class Program
     {
+        private static TelegramBotClient Bot;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.ReadKey();
+            Bot = new TelegramBotClient("Your API key");
+            var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            Bot.OnMessage += BotOnMessageReceived;
+
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+            Bot.StopReceiving();
         }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.TextMessage) return;
+            switch (message.Text.Split(' ').First())
+            {
+                // send inline keyboard
+                
+                default:
+                    const string usage = @"
+                                        Usage:
+                                        /inline   - send inline keyboard
+                                        /keyboard - send custom keyboard
+                                        /photo    - send a photo
+                                        /request  - request location or contact";
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        usage,
+                        replyMarkup: new ReplyKeyboardRemove());
+                    break;
+            }
+}
     }
 }
